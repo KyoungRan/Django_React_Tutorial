4cbcd28acadac8d56fe513a3cb70e58d44115656
KyoungRan <kyoungrankang@gmail.com>
2017-02-02 00:29:43 +0900

    deleted

--- 02-NoobMusic/myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/cygdrive/e/workspaces/Django_React_Tutorial/02-NoobMusic/myvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
